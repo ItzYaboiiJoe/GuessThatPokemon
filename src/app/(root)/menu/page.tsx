01e876b4b5b36da3523c90dfbe83a9c864b5e09e
Joseph Seoudy <110504568+ItzYaboiiJoe@users.noreply.github.com>
2025-09-10 16:53:03 -0400

    Created a Menu Page

---
+++ src/app/(root)/menu/page.tsx
@@ -0,0 +1,11 @@
+const Menu = () => {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center">
+      <div className="bg-white/90 rounded-xl shadow-xl p-8 w-full max-w-md text-center">
+        <h1>Menu Page</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Menu;
