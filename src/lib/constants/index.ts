fd85b99a421ed4ac7a1860a7b1ad586482ed582c
Joseph Seoudy <110504568+ItzYaboiiJoe@users.noreply.github.com>
2025-09-10 23:10:50 -0400

    Renamed App Name

--- src/lib/constants/index.ts
+++ src/lib/constants/index.ts
@@ -1,3 +1,3 @@
-export const APP_NAME = "Guess That Pokemon";
+export const APP_NAME = "Guess What - Poki Edition";
 export const APP_DESCRIPTION =
   "A simple game app to guess what is the pokemon shown in the image.";
