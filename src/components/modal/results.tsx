155d0d68bbdee8fa46266ee2ba999b2c3fb0c19a
Joseph Seoudy <110504568+ItzYaboiiJoe@users.noreply.github.com>
2025-09-18 23:02:46 -0400

    Created a template skeleton modal file as a setup for creating the results modal

---
+++ src/components/modal/results.tsx
@@ -0,0 +1,41 @@
+// This file will display the results modal when a user submits an answer
+// It will display either correct or incorrect
+// It will also display the results when the user already submitted the answer for the day
+
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import { Button } from "../ui/button";
+
+const Results = () => {
+  return (
+    <Dialog>
+      <DialogContent
+        className="w-96 rounded-2xl shadow-2xl bg-gradient-to-b from-yellow-100 via-orange-100 to-white"
+        onInteractOutside={(e) => e.preventDefault()}
+        onEscapeKeyDown={(e) => e.preventDefault()}
+      >
+        <DialogHeader className="text-center">
+          <DialogTitle className="text-center">Test Title</DialogTitle>
+          <DialogDescription className="text-center">
+            Test Description
+          </DialogDescription>
+        </DialogHeader>
+
+        <div>
+          <span>Insert Hints and pokemon types here</span>
+        </div>
+
+        <div className="mt-6 flex justify-center">
+          <Button>Close</Button>
+        </div>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default Results;
