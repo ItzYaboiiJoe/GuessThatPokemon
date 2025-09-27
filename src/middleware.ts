fbadc09ae108e1b259ec42ece564f6a564b87b3a
Joseph Seoudy <110504568+ItzYaboiiJoe@users.noreply.github.com>
2025-09-26 23:56:02 -0400

    Emergency fix V.0.6.1 Created a Middleware to prevent users from routing into auth users menu pages and game which will use cached credentials from the last logged in user

---
+++ src/middleware.ts
@@ -0,0 +1,13 @@
+import { NextResponse, NextRequest } from "next/server";
+
+export function middleware(request: NextRequest) {
+  if (request.nextUrl.pathname === "/") {
+    return NextResponse.next();
+  }
+
+  return NextResponse.redirect(new URL("/", request.url));
+}
+
+export const config = {
+  matcher: ["/menu", "/game", "/guest"],
+};
