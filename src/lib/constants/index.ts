9fc0f38bf532c16b6348e08c223da3c3cee6c2f9
Joseph Seoudy <110504568+ItzYaboiiJoe@users.noreply.github.com>
2025-10-25 00:59:06 -0400

    Setup new version branch

--- src/lib/constants/index.ts
+++ src/lib/constants/index.ts
@@ -1,5 +1,5 @@
 export const APP_NAME = "Guess What - Poki Edition";
 export const APP_DESCRIPTION =
   "A simple game app to guess what is the pokemon shown in the image.";
-export const APP_VERSION = "Version 0.7.6 (Beta)";
+export const APP_VERSION = "Version 0.8.0 (Beta)";
 export const SITE_URL = "https://guesswhat-pokemon.vercel.app";
