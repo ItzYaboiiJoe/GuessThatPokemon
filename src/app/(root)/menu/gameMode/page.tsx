f34ce25b99d843bfeabfe7b5819cc70edae3dccb
Joseph Seoudy <110504568+ItzYaboiiJoe@users.noreply.github.com>
2025-10-25 01:23:17 -0400

    Added Return to Menu button

--- src/app/(root)/menu/gameMode/page.tsx
+++ src/app/(root)/menu/gameMode/page.tsx
@@ -1,3 +1,6 @@
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+
 const GameMode = () => {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center">
@@ -12,6 +15,15 @@ const GameMode = () => {
           <h1 className="text-3xl font-bold mb-6">Region Game Mode</h1>
         </div>
       </div>
+
+      {/* Back to Home Button */}
+      <Button
+        asChild
+        variant={"default"}
+        className="mt-6 bg-red-600 hover:bg-red-800"
+      >
+        <Link href="/menu">Return to Menu</Link>
+      </Button>
     </div>
   );
 };
