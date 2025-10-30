3ce56a474a5c039308bae04b513bcc409f925029
Joseph Seoudy <110504568+ItzYaboiiJoe@users.noreply.github.com>
2025-10-29 21:13:10 -0400

    Created a return view for Region Game Page

---
+++ src/app/(root)/regionGame/page.tsx
@@ -0,0 +1,9 @@
+const RegionGame = () => {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-between bg-gradient-to-b from-indigo-900 via-purple-800 to-black text-white p-6">
+      <h1>Region Game</h1>
+    </div>
+  );
+};
+
+export default RegionGame;
