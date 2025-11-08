989220a1d1a59ad3dc91904e39aae0ae8652e3de
Joseph Seoudy <110504568+ItzYaboiiJoe@users.noreply.github.com>
2025-11-08 18:59:42 -0500

    Installed a new component ElectricBorder, will need more work to make it look better

--- src/app/(root)/menu/gameMode/page.tsx
+++ src/app/(root)/menu/gameMode/page.tsx
@@ -13,6 +13,7 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import ElectricBorder from "@/components/bitsComponents/ElectricBorder";
 
 const GameMode = () => {
   // State to hold Region Pokemon
@@ -54,70 +55,74 @@ const GameMode = () => {
 
       <div className="grid grid-cols-1 md:grid-cols-2 gap-10">
         {/* National Mode */}
-        <Card className="bg-white/90 rounded-3xl shadow-xl shadow-orange-400/50 p-8 w-full max-w-sm border-1 border-yellow-600">
-          <CardHeader>
-            {/* Countdown */}
-            <div className="flex justify-center mb-4">
-              <div className="flex items-center gap-2 bg-gradient-to-r from-yellow-400 to-orange-400 text-black font-semibold px-4 py-1 rounded-full shadow-md border border-orange-300">
-                <p>New Challenge In:</p>
-                <CountdownTimer targetTime="00:00" />
+        <ElectricBorder color="#b37100" speed={2} chaos={0.5} thickness={3}>
+          <Card className="bg-white/90 rounded-3xl shadow-xl shadow-orange-400/50 p-8 w-full max-w-sm border-1 border-yellow-600">
+            <CardHeader>
+              {/* Countdown */}
+              <div className="flex justify-center mb-4">
+                <div className="flex items-center gap-2 bg-gradient-to-r from-yellow-400 to-orange-400 text-black font-semibold px-4 py-1 rounded-full shadow-md border border-orange-300">
+                  <p>New Challenge In:</p>
+                  <CountdownTimer targetTime="00:00" />
+                </div>
               </div>
-            </div>
 
-            <CardTitle className="text-2xl font-bold mb-2 text-transparent bg-clip-text bg-gradient-to-r from-yellow-500 to-orange-500">
-              National Game Mode
-            </CardTitle>
-            <CardDescription className="text-gray-600">
-              Good Luck, Have Fun!
-            </CardDescription>
-          </CardHeader>
-          <CardContent>
-            <Button
-              className="w-full bg-gradient-to-r from-yellow-300 to-amber-500 text-black font-bold rounded-full shadow-md shadow-yellow-200/70 hover:from-yellow-400 hover:to-amber-600 transition-all duration-300"
-              asChild
-            >
-              <Link href="/nationGame">Start Game</Link>
-            </Button>
-            <p className="mt-4 text-sm text-gray-600">
-              National Game Mode affects leaderboard scores
-            </p>
-          </CardContent>
-        </Card>
+              <CardTitle className="text-2xl font-bold mb-2 text-transparent bg-clip-text bg-gradient-to-r from-yellow-500 to-orange-500">
+                National Game Mode
+              </CardTitle>
+              <CardDescription className="text-gray-600">
+                Good Luck, Have Fun!
+              </CardDescription>
+            </CardHeader>
+            <CardContent>
+              <Button
+                className="w-full bg-gradient-to-r from-yellow-300 to-amber-500 text-black font-bold rounded-full shadow-md shadow-yellow-200/70 hover:from-yellow-400 hover:to-amber-600 transition-all duration-300"
+                asChild
+              >
+                <Link href="/nationGame">Start Game</Link>
+              </Button>
+              <p className="mt-4 text-sm text-gray-600">
+                National Game Mode affects leaderboard scores
+              </p>
+            </CardContent>
+          </Card>
+        </ElectricBorder>
 
         {/* Regional Mode */}
-        <Card className="bg-white/90 rounded-3xl shadow-xl shadow-red-400/50 p-8 w-full max-w-sm border-1 border-red-500">
-          <CardHeader>
-            {/* Countdown */}
-            <div className="flex justify-center mb-4">
-              <div className="flex items-center gap-1 bg-gradient-to-r from-orange-300 to-red-400 text-black font-semibold px-4 py-1 rounded-full shadow-md border border-red-200">
-                <p>New Challenge In:</p>
-                <CountdownTimer targetTime="00:00" />
+        <ElectricBorder color="#fb2c36" speed={2} chaos={0.5} thickness={3}>
+          <Card className="bg-white/90 rounded-3xl shadow-xl shadow-red-400/50 p-8 w-full max-w-sm border-1 border-red-500">
+            <CardHeader>
+              {/* Countdown */}
+              <div className="flex justify-center mb-4">
+                <div className="flex items-center gap-1 bg-gradient-to-r from-orange-300 to-red-400 text-black font-semibold px-4 py-1 rounded-full shadow-md border border-red-200">
+                  <p>New Challenge In:</p>
+                  <CountdownTimer targetTime="00:00" />
+                </div>
               </div>
-            </div>
 
-            <CardTitle className="text-2xl font-bold mb-2 text-transparent bg-clip-text bg-gradient-to-r from-orange-400 to-red-700">
-              Region Game Mode
-            </CardTitle>
-            <CardDescription className="text-gray-700">
-              Current Active Region:{" "}
-              <span className="font-extrabold text-orange-600 drop-shadow-[0_0_6px_rgba(255,180,80,0.7)]">
-                {region}
-              </span>
-            </CardDescription>
-          </CardHeader>
-          <CardContent>
-            <Button
-              asChild
-              className="w-full bg-gradient-to-r from-orange-400 to-red-500 text-white font-bold rounded-full shadow-md shadow-orange-200/70 hover:from-orange-500 hover:to-red-600 transition-all duration-300"
-            >
-              <Link href="/regionGame">Start Game</Link>
-            </Button>
+              <CardTitle className="text-2xl font-bold mb-2 text-transparent bg-clip-text bg-gradient-to-r from-orange-400 to-red-700">
+                Region Game Mode
+              </CardTitle>
+              <CardDescription className="text-gray-700">
+                Current Active Region:{" "}
+                <span className="font-extrabold text-orange-600 drop-shadow-[0_0_6px_rgba(255,180,80,0.7)]">
+                  {region}
+                </span>
+              </CardDescription>
+            </CardHeader>
+            <CardContent>
+              <Button
+                asChild
+                className="w-full bg-gradient-to-r from-orange-400 to-red-500 text-white font-bold rounded-full shadow-md shadow-orange-200/70 hover:from-orange-500 hover:to-red-600 transition-all duration-300"
+              >
+                <Link href="/regionGame">Start Game</Link>
+              </Button>
 
-            <p className="mt-4 text-sm text-gray-600">
-              Region Game Mode does not affect leaderboard
-            </p>
-          </CardContent>
-        </Card>
+              <p className="mt-4 text-sm text-gray-600">
+                Region Game Mode does not affect leaderboard
+              </p>
+            </CardContent>
+          </Card>
+        </ElectricBorder>
       </div>
 
       {/* Return to Menu */}
